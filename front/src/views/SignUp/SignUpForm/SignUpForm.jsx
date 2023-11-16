4108d8c0f6cf73ec641ce2541e2fc7b6daa7b7d4
Keegan Elliott <keegan@robotire.com>
2023-11-16 15:38:21 -0500

    Sign up form now works on browser

--- front/src/views/SignUp/SignUpForm/SignUpForm.jsx
+++ front/src/views/SignUp/SignUpForm/SignUpForm.jsx
@@ -1,9 +1,46 @@
+import { useState } from 'react';
 import Button from 'react-bootstrap/Button';
 import FloatingLabel from 'react-bootstrap/FloatingLabel';
 import Form from 'react-bootstrap/Form';
+import { ROUTES } from 'utils/routes';
 
 
 export function SignUpForm() {
+    const [email, setEmail] = useState('');
+    const [password, setPassword] = useState('');
+
+    const handleEmailChange = (e) => {
+        setEmail(e.target.value);
+    };
+
+    const handlePasswordChange = (e) => {
+        setPassword(e.target.value);
+    };
+
+    const handleSubmit = async (e) => {
+        // e.preventDefault();
+        const data = {
+            email: email,
+            password: password
+        };
+        console.log(`Sending POST request to http://localhost:3000${ROUTES.SIGNUP}`);
+        const response = await fetch(`http://localhost:3000${ROUTES.SIGNUP}`, {
+            method: 'POST',
+            headers: {
+                'Content-Type': 'application/json'
+            },
+            body: JSON.stringify(data)
+        });
+        console.log(response);
+        const json = await response.json();
+        console.log(json);
+        if (response.ok) {
+            alert(`User, ${email}, is now registered! (Don't forget your password... You can't reset it!)`)
+        } else {
+            alert(`User, ${email}, already exists! Please log in instead. (Forgot your password? Sorry. You'll have to create a new account.)`)
+        }
+    };
+
     return (
         <div className="w-25">
             <br />
@@ -12,13 +49,13 @@ export function SignUpForm() {
                 label='Email address'
                 className='mb-3'
             >
-                <Form.Control type='email' placeholder='name@example.com' />
+                <Form.Control type='email' placeholder='name@example.com' value={email} onChange={handleEmailChange} />
             </FloatingLabel>
             <FloatingLabel controlId="floatingPassword" label="Password">
-                <Form.Control type="password" placeholder="Password" />
+                <Form.Control type="password" placeholder="Password" value={password} onChange={handlePasswordChange} />
             </FloatingLabel>
             <br />
-            <Button as='input' type='submit' value='Sign up' />
+            <Button as='input' type='submit' value='Sign up' onClick={handleSubmit} />
         </div>
     )
 }
